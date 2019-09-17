156d148204ad2c69c6028d0705873904b5db97d5
Javier Lete <javier@eskura21.com>
2019-09-17 20:41:06 +0200

    Listado funcional con REST

---
+++ src/app/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from '../persona';
+import { PersonaService } from '../persona.service';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  personas: Persona[];
+
+  constructor(private personaService: PersonaService) { }
+
+  ngOnInit() {
+    this.personaService.getPersonas().subscribe(
+      personas => this.personas = personas);
+  }
+}
---
+++ src/app/persona.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Persona } from './persona';
+
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+
+  private url = 'http://localhost:3000/personas/';
+
+  constructor(private http: HttpClient) {}
+
+  personas: Persona[];
+
+  getPersonas(): Observable<Persona[]> {
+    return this.http.get<Persona[]>(this.url);
+  }
+}
