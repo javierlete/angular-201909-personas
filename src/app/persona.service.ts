c9c592fb5df1158e17af7fe61725fc0eaafa226a
Javier Lete <javier@eskura21.com>
2019-09-25 19:48:19 +0200

    SnackBar para mensajes de error y correctos

--- src/app/persona.service.ts
+++ src/app/persona.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { Persona } from './persona';
 
 import { HttpClient } from '@angular/common/http';
@@ -29,20 +29,31 @@ export class PersonaService {
 
   postPersona(persona: Persona): Observable<Persona> {
     return this.http.post<Persona>(this.url, persona).pipe(
+      this.tapTostada('Se ha añadido a ', persona.nombre),
       catchError(this.gestionarError<Persona>('Alta de persona', persona))
     );
   }
 
   putPersona(persona: Persona): Observable<Persona> {
     return this.http.put<Persona>(this.url + persona.id, persona).pipe(
+      this.tapTostada('Se ha modificado ', persona.nombre),
       catchError(this.gestionarError<Persona>('Modificación de persona', persona))
     );
   }
 
   deletePersona(id: number): Observable<Persona> {
     return this.http.delete<Persona>(this.url + id).pipe(
+      this.tapTostada('Se ha borrado la persona cuyo id es ', id),
       catchError(this.gestionarError<Persona>('Borrado de persona', new Persona()))
     );
+  }
+
+  private abrirTostada(mensaje: string, dato: number | string, boton: string = 'Cerrar', duracion: number = 0) {
+    return this.snackBar.open(mensaje + dato, boton, { duration: duracion });
+  }
+
+  private tapTostada(mensaje: string, dato: number | string) {
+    return tap<Persona>(_ => this.abrirTostada(mensaje, dato, 'Cerrar', 3000));
   }
 
   private gestionarError<T>(operation = 'operation', result?: T) {
@@ -50,7 +61,7 @@ export class PersonaService {
 
       console.error(operation, error);
 
-      this.snackBar.open(operation + ' ha fallado', 'Cerrar');
+      this.abrirTostada(operation + ' ha fallado', 'Cerrar');
 
       return of(result as T);
     };
